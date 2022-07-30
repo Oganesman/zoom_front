27044bc1a51a0c80ce1928d0deaf4fb8a51076be
Oganesman <oganespetikyan@gmail.com>
2022-07-30 22:19:50 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,78 +13,78 @@ ZoomMtg.i18n.load('en-US');
 ZoomMtg.i18n.reload('en-US');
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
-  // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
-  signatureEndpoint = ''
-  // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
-  sdkKey = ''
-  meetingNumber = '123456789'
-  role = 0
-  leaveUrl = 'http://localhost:4200'
-  userName = 'Angular'
-  userEmail = ''
-  passWord = ''
-  // pass in the registrant's token if your meeting or webinar requires registration. More info here:
-  // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-registered
-  // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-registered
-  registrantToken = ''
+	// setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
+	signatureEndpoint = 'http://localhost:4000'
+	// This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
+	sdkKey = 'x4EmVHj9ELqZpt2xQrnIXdntWYNpqetrR8K3'
+	meetingNumber = '81680779771'
+	role = 0
+	leaveUrl = 'http://localhost:4200'
+	userName = 'Angular'
+	userEmail = ''
+	passWord = ''
+	// pass in the registrant's token if your meeting or webinar requires registration. More info here:
+	// Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-registered
+	// Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-registered
+	registrantToken = ''
 
-  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {
+	constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {
 
-  }
+	}
 
-  ngOnInit() {
+	ngOnInit() {
 
-  }
+	}
 
-  getSignature() {
-    this.httpClient.post(this.signatureEndpoint, {
-	    meetingNumber: this.meetingNumber,
-	    role: this.role
-    }).toPromise().then((data: any) => {
-      if(data.signature) {
-        console.log(data.signature)
-        this.startMeeting(data.signature)
-      } else {
-        console.log(data)
-      }
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
+	getSignature() {
+		this.httpClient.post(this.signatureEndpoint, {
+			meetingNumber: this.meetingNumber,
+			role: this.role
+		}).toPromise().then((data: any) => {
+			if (data.signature) {
+				console.log(data.signature)
+				this.startMeeting(data.signature)
+			} else {
+				console.log(data)
+			}
+		}).catch((error) => {
+			console.log(error)
+		})
+	}
 
-  startMeeting(signature) {
+	startMeeting(signature) {
 
-    document.getElementById('zmmtg-root').style.display = 'block'
+		document.getElementById('zmmtg-root').style.display = 'block'
 
-    ZoomMtg.init({
-      leaveUrl: this.leaveUrl,
-      success: (success) => {
-        console.log(success)
-        ZoomMtg.join({
-          signature: signature,
-          meetingNumber: this.meetingNumber,
-          userName: this.userName,
-          sdkKey: this.sdkKey,
-          userEmail: this.userEmail,
-          passWord: this.passWord,
-          tk: this.registrantToken,
-          success: (success) => {
-            console.log(success)
-          },
-          error: (error) => {
-            console.log(error)
-          }
-        })
-      },
-      error: (error) => {
-        console.log(error)
-      }
-    })
-  }
+		ZoomMtg.init({
+			leaveUrl: this.leaveUrl,
+			success: (success) => {
+				console.log(success)
+				ZoomMtg.join({
+					signature: signature,
+					meetingNumber: this.meetingNumber,
+					userName: this.userName,
+					sdkKey: this.sdkKey,
+					userEmail: this.userEmail,
+					passWord: this.passWord,
+					tk: this.registrantToken,
+					success: (success) => {
+						console.log(success)
+					},
+					error: (error) => {
+						console.log(error)
+					}
+				})
+			},
+			error: (error) => {
+				console.log(error)
+			}
+		})
+	}
 }
